b788196681a5bc99864cf17d666c613f1ffcd0fb
Haruki <75521368+Haruki-Kawazoe@users.noreply.github.com>
2022-07-27 09:44:20 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,50 +3,50 @@ function 初期設定 () {
     radio.setGroup(1)
     basic.showString("This is Traffic Light for micro:bit !!BETA!! ")
 }
-function ボタンAが押されたとき () {
+function normalstartが入力された時 () {
     動作開始 = 1
     led.enable(false)
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    pins.digitalWritePin(DigitalPin.P1, 1)
-    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
     // hokohsya
-    pins.digitalWritePin(DigitalPin.P3, 1)
+    pins.digitalWritePin(DigitalPin.P3, 0)
     // hokohsya
-    pins.digitalWritePin(DigitalPin.P4, 1)
+    pins.digitalWritePin(DigitalPin.P4, 0)
     while (動作開始 == 1) {
-        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P0, 1)
         // hokohsya
-        pins.digitalWritePin(DigitalPin.P3, 0)
+        pins.digitalWritePin(DigitalPin.P3, 1)
         basic.pause(25000)
         // hokohsya
         for (let index = 0; index < 9; index++) {
             // hokohsya
-            pins.digitalWritePin(DigitalPin.P3, 1)
+            pins.digitalWritePin(DigitalPin.P3, 0)
             // hokohsya
             basic.pause(250)
             // hokohsya
-            pins.digitalWritePin(DigitalPin.P3, 0)
+            pins.digitalWritePin(DigitalPin.P3, 1)
             // hokohsya
             basic.pause(250)
         }
         // hokohsya
-        pins.digitalWritePin(DigitalPin.P3, 1)
-        // hokohsya
-        pins.digitalWritePin(DigitalPin.P4, 0)
-        basic.pause(1000)
-        pins.digitalWritePin(DigitalPin.P0, 1)
-        pins.digitalWritePin(DigitalPin.P1, 0)
-        basic.pause(3000)
-        pins.digitalWritePin(DigitalPin.P1, 1)
-        pins.digitalWritePin(DigitalPin.P2, 0)
-        basic.pause(30000)
-        pins.digitalWritePin(DigitalPin.P2, 1)
+        pins.digitalWritePin(DigitalPin.P3, 0)
         // hokohsya
         pins.digitalWritePin(DigitalPin.P4, 1)
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(3000)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        basic.pause(30000)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        // hokohsya
+        pins.digitalWritePin(DigitalPin.P4, 0)
     }
 }
-input.onButtonPressed(Button.A, function () {
-    ボタンAが押されたとき()
+serial.onDataReceived("normalstart", function () {
+    normalstartが入力された時()
 })
 input.onButtonPressed(Button.B, function () {
 	
